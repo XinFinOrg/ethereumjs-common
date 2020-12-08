eab44938ea6a16280f3a4c84eef944beb1a30bd3
AnilChinchawale <anil24593@gmail.com>
2020-12-08 12:31:53 +0530

    dist added

---
+++ dist/chains/index.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.chains = void 0;
+exports.chains = {
+    names: {
+        '1': 'mainnet',
+        '3': 'ropsten',
+        '4': 'rinkeby',
+        '42': 'kovan',
+        '5': 'goerli',
+        '50': 'xdcmainnet',
+        '51': 'apothem',
+    },
+    mainnet: require('./mainnet.json'),
+    ropsten: require('./ropsten.json'),
+    rinkeby: require('./rinkeby.json'),
+    kovan: require('./kovan.json'),
+    goerli: require('./goerli.json'),
+    xdcmainnet: require('./xdcmainnet.json'),
+    apothem: require('./apothem.json'),
+};
+//# sourceMappingURL=index.js.map
